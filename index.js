88fc66d82a5e669fc88e5b268708bc89511fa9c8
Tucker Leach <leachtucker@gmail.com>
2020-08-13 13:23:20 -0700

    Finish task 6

--- index.js
+++ index.js
@@ -73,9 +73,11 @@ and should return a string in the format `{name} gave the restaurant a {rating},
  * it will return `Daniela gave the restaurant a 5 star review and their feedback was: Beautiful atmosphere and wonderful vegan options!`
 */
 function getReviewByIndex(reviews, index) {
-    /* code here */
+    let review = reviews[index];
+    return `${review.name} gave the restaurant a ${review.rating} star review and their feedback was: ${review.feedback}`
 }
 
+console.log(getReviewByIndex(reviews, 0));
 
 /* Task 7: Write a function to get information about the most recent review called `getLastReview`
 
